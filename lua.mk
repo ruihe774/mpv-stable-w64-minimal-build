5d335f48fa366e8908cd43521d13bb448c5565fa
Misaki Kasumi <misakikasumi@outlook.com>
2021-03-19 01:35:59 +0800

    make lua dll

--- lua.mk
+++ lua.mk
@@ -1,6 +1,6 @@
 include Prelude.mk
 
-SUB_MAKE = $(MAKE) -C lua
+SUB_MAKE = $(MAKE) -C lua INSTALL_TOP=$(PREFIX) CC=x86_64-w64-mingw32-gcc TO_BIN=lua52.dll TO_LIB=liblua.dll.a
 DOWNLOADS = downloads
 UNARCHIVED = $(DOWNLOADS)/lua-5.2.4
 ARCHIVE = $(DOWNLOADS)/lua-5.2.4.tar.gz
@@ -12,8 +12,9 @@ dist/lua52.dll: buildroot/bin/lua52.dll
 	$(STRIP) $< -o $@
 
 buildroot/bin/lua52.dll: lua
-	$(SUB_MAKE) INSTALL_TOP=$(PREFIX) CC=x86_64-w64-mingw32-gcc TO_BIN=lua52.dll mingw
-	$(SUB_MAKE) INSTALL_TOP=$(PREFIX) CC=x86_64-w64-mingw32-gcc TO_BIN=lua52.dll install
+	$(SUB_MAKE) mingw
+	cd lua/src && x86_64-w64-mingw32-gcc -Wl,--out-implib=liblua.dll.a -shared -o lua52.dll lapi.o lcode.o lctype.o ldebug.o ldo.o ldump.o lfunc.o lgc.o llex.o lmem.o lobject.o lopcodes.o lparser.o lstate.o lstring.o ltable.o ltm.o lundump.o lvm.o lzio.o lauxlib.o lbaselib.o lbitlib.o lcorolib.o ldblib.o liolib.o lmathlib.o loslib.o lstrlib.o ltablib.o loadlib.o linit.o	# fuck!
+	$(SUB_MAKE) install
 
 buildroot/lib/pkgconfig/lua.pc:
 	cp lua.files/lua.pc $@

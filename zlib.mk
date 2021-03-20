43e8ec666467e72e0640fa667a6d33f0048ebff1
Misaki Kasumi <misakikasumi@outlook.com>
2021-03-21 03:30:02 +0800

    fix zlib.mk

--- zlib.mk
+++ zlib.mk
@@ -2,8 +2,8 @@ ZLIB_PATH = /usr/x86_64-w64-mingw32/lib/zlib1.dll
 
 include Prelude.mk
 
-dist: $(DIST)/iconv.dll
-$(DIST)/iconv.dll:
+dist: $(DIST)/zlib1.dll
+$(DIST)/zlib1.dll:
 	cp $(ZLIB_PATH) $@
 
 build:

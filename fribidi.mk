fb20697ea1b8a7c4babcb9497addafc4939e29bb
Misaki Kasumi <misakikasumi@outlook.com>
2021-03-23 14:25:21 +0800

    use meson for fribidi

--- fribidi.mk
+++ fribidi.mk
@@ -1,23 +1,9 @@
 DLL_NAME = libfribidi-0.dll
+MESON_OPTIONS =\
+--buildtype=release\
+-Ddeprecated=false\
+-Ddocs=false\
+-Dbin=false\
+-Dtests=false
 
 include Prelude.mk
-
-build: $(BIN_DLL)
-
-$(BIN_DLL): $(PKG_SRC)/Makefile FORCE
-	+$(SUB_MAKE)
-	+$(SUB_MAKE) install
-
-$(PKG_SRC)/Makefile: $(PKG_SRC)/configure
-	cd $(PKG_SRC) && ./configure
-	+$(SUB_MAKE)
-	$(SUB_CONFIGURE)
-
-$(PKG_SRC)/configure:
-	cd $(PKG_SRC) && NOCONFIGURE=1 ./autogen.sh
-
-clean:
-	+$(SUB_MAKE) clean
-
-distclean:
-	+$(SUB_MAKE) distclean

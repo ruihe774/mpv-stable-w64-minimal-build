bbc18f172522342ca0385f52720fd5bcabdc1d67
Misaki Kasumi <misakikasumi@outlook.com>
2024-01-26 16:21:29 +0800

    fix

--- harfbuzz.mk
+++ harfbuzz.mk
@@ -2,11 +2,10 @@ DLL_NAME = libharfbuzz-0.dll
 MESON_OPTIONS =\
 --buildtype=minsize\
 -Dtests=disabled\
--Ddirectwrite=enabled\
 -Dfreetype=disabled\
 '-Dcpp_args=\
   -DHB_TINY\
-  -DHB_NO_OT\
+  -DHB_NO_OT_FONT\
   -DHB_NO_UCD\
   -DHB_NO_UNICODE_FUNCS'
 

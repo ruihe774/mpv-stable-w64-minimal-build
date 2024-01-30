5417d5741c2f65b70ec6bdcb886a65738f420dcd
Misaki Kasumi <misakikasumi@outlook.com>
2024-01-31 05:26:41 +0800

    ffmpeg stub

--- ffmpeg.mk
+++ ffmpeg.mk
@@ -2,10 +2,6 @@ NO_DIST = 1
 
 include Prelude.mk
 
-UNARCHIVED = $(DOWNLOADS)/ffmpeg
-ARCHIVE = $(DOWNLOADS)/ffmpeg.7z
-URL = https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full-shared.7z
-
 libs = libavformat libavcodec libavutil libavfilter libswresample libswscale
 implib = $(addsuffix .dll.a, $(libs))
 pc = $(addsuffix .pc, $(libs))
@@ -15,27 +11,9 @@ header_path = $(addprefix $(INCLUDE)/,$(libs))
 
 build: $(implib_path) $(pc_path) $(header_path)
 
-$(implib_path): $(UNARCHIVED)
-	cp $</lib/$(notdir $@) $@
+$(implib_path) $(header_path) $(pc_path) &: $(PKG_SRC)/config.h
+	+$(SUB_MAKE)
+	+$(SUB_MAKE) install
 
-$(header_path): $(UNARCHIVED)
-	cp -r $</include/$(notdir $@) $@
-
-$(pc_path):
-	cp $(SRC)/ffmpeg.files/$(notdir $@) $@
-
-$(UNARCHIVED): $(ARCHIVE)
-	-rm -r $(UNARCHIVED) $(DOWNLOADS)/ffmpeg-*-full_build-shared
-	cd $(DOWNLOADS) && p7zip -d -k $(notdir $<)
-	cd $(DOWNLOADS) && mv -T ffmpeg-*-full_build-shared $(notdir $@)
-	touch $@
-
-$(ARCHIVE):
-	wget '$(URL)' -O $(addsuffix .tmp,$@)
-	mv $(addsuffix .tmp,$@) $@
-
-clean:
-	-rm -r $(pc_path) $(header_path) $(implib_path)
-
-distclean: clean
-	-rm -r $(UNARCHIVED) $(ARCHIVE)
+$(PKG_SRC)/config.h:
+	cd $(PKG_SRC) && ./configure --arch=x86_64 --target-os=mingw32 --cross-prefix=$(HOST)- --disable-everything --enable-shared --prefix=$(PREFIX)
